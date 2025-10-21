cbc76775ff5381503b5d268bff5eb8983b27d093
sitahald <sitahald@sheridancollege.ca>
2025-10-20 21:02:00 -0400

    second commit

--- practice/src/main/java/git/practice/Practice.java
+++ practice/src/main/java/git/practice/Practice.java
@@ -11,6 +11,6 @@ package git.practice;
 public class Practice {
 
     public static void main(String[] args) {
-        System.out.println("practice 1");
+        System.out.println("practice 2");
     }
 }
